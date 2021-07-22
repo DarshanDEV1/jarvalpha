356c7b3a5e1312acabee22de437cd3645eabf894
DarshanDEV1 <86543754+DarshanDEV1@users.noreply.github.com>
2021-07-22 08:23:22 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import pyttsx3
+arnab = pyttsx3.init()
+speech=input("JARVALPHA IS ACTIVE NOW::-")
+if speech: arnab.say("hello there")
+arnab.runAndWait()
+while True :
+    import pyttsx3
+
+    arnab = pyttsx3.init()
+    speech = input("JARVALPHA IS ACTIVE NOW::-")
+    if speech: arnab.say("I am fine ,  what about you?")
+    arnab.runAndWait()
+    while True :
+        import pyttsx3
+
+        arnab = pyttsx3.init()
+        speech = input("JARVALPHA IS ACTIVE NOW::-")
+        if speech: arnab.say("my self jarvalpha version 3.o , built by darshan.   can i know what is your name , Gentle man?")
+        arnab.runAndWait()
+        while True :
+            import pyttsx3
+
+            arnab = pyttsx3.init()
+            speech = input("JARVALPHA IS ACTIVE NOW::-")
+            if speech: arnab.say("WOW nice name.  You are so lucky that you are human and can give birth to lakhs of jarvalpha robots AI like me.  You humans are so intelligent")
+            arnab.runAndWait()
+            while True:
+                import pyttsx3
+
+                arnab = pyttsx3.init()
+                speech = input("JARVALPHA IS ACTIVE NOW::-")
+                if speech: arnab.say("yahh ur most welcome   but please use your brains in right manner dont use it to do wrong things,  like cybercrime and all ETC")
+                arnab.runAndWait()
+                while True:
+                    import pyttsx3
+
+                    arnab = pyttsx3.init()
+                    speech = input("JARVALPHA IS ACTIVE NOW::-")
+                    if speech: arnab.say("Thanks for your cooperation to me.  Thats all dat programmed in me for now ,  meet you soon after my  boss  darshan update new program ,  GOOD BYE ")
+                    arnab.runAndWait()
+
+
